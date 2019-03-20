9b6f263482b0b0087c4cb3cc7bc35b158260afd5
Austin Kim <austinskim@gmail.com>
2019-03-19 21:13:32 -0700

    Update settings.py to use PostgreSQL

--- server/prayersite/prayersite/settings.py
+++ server/prayersite/prayersite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'prayersite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'prayer_wall',
+	'USER': 'testuer',
+	'PASSWORD': 'test',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
